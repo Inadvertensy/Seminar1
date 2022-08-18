fadbf934ea7a32caedd2dfe1889762878e5f0d8b
1 1 <bobra2@ya.ru>
2022-08-18 22:19:51 +0300

    Example5 added

---
+++ Example 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter Number");
+int number=Convert.ToInt32(Console.ReadLine());
+int number2=-number;
+while(number2<=number){
+    //Console.WriteLine($"{number2},")
+    Console.WriteLine(number2+",");
+    ++number2;
+}
+
---
+++ Example 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number");
+
+int number=Convert.ToInt32(Console.ReadLine());
+
+if (((number/100)>0) && ((number/1000)<1) ){
+int lastNumber=number%10;
+Console.WriteLine("Last number:   " + lastNumber);
+}
+else {
+    Console.WriteLine("Bad Number");
+
+}
