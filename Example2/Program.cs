6317f4ea56a2169c6d47f597d405c360a4f54dcf
1 1 <bobra2@ya.ru>
2022-08-18 21:22:04 +0300

    Example 2 added

---
+++ Example2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Please enter value 1");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter value 2");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+
+int squareNumber2=number2*number2;
+//Console.WriteLine(squareNumber2);
+
+
+if (number1==squareNumber2)
+{
+    Console.WriteLine("yes");
+}
+else {
+    
+    Console.WriteLine("no");
+}
