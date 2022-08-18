7ce3d88107916bdadf57b7b3d7d8434f40b0ca1a
1 1 <bobra2@ya.ru>
2022-08-18 21:01:11 +0300

    first example

---
+++ Example1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Write a number please");
+int number= Convert.ToInt32( Console.ReadLine());
+int square=number*number;
+Console.WriteLine("Square numberrrr isss    "+square);
+
