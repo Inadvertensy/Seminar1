81c6585fbe581dc58fca439dd42db0418888825b
1 1 <bobra2@ya.ru>
2022-08-18 21:53:41 +0300

    Example 3 updated to teacher's

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,18 +1,32 @@
 ﻿Console.Clear();
 Console.WriteLine("Please enter numer");
 int number=Convert.ToInt32(Console.ReadLine());
-if (number==3)
+// if (number==3)
+// {
+// Console.WriteLine("Wednesday");
+// }
+// else if (number==5) 
+// {
+//    Console.WriteLine("Friday");
+// }
+// else
+// {
+//     Console.WriteLine("Neither");
+// }
 
+if (number>=1 && number <=7)
 {
-Console.WriteLine("Wednesday");
+switch(number)
+{
+case 1:
+System.Console.WriteLine("Monday"); break;
+case 2: System.Console.WriteLine("Tuesday"); break;
+case 3: System.Console.WriteLine("Wednesday"); break;
+case 4: System.Console.WriteLine("Thursday"); break;
+case 5: System.Console.WriteLine("Friday"); break;
+case 6: System.Console.WriteLine("Saturday"); break;
+case 7: System.Console.WriteLine("Sunday"); break;
+default: System.Console.WriteLine("Bad number"); break;
 }
 
-if (number==5) 
-{
-    Console.WriteLine("Friday");
-
-}
-else
-{
-    Console.WriteLine("Neither");
 }
