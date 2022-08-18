4046b1ac2c7c4847437f175cf1e8742965e32da4
1 1 <bobra2@ya.ru>
2022-08-18 21:41:20 +0300

    Example3 written

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Please enter numer");
+int number=Convert.ToInt32(Console.ReadLine());
+if (number==3)
+
+{
+Console.WriteLine("Wednesday");
+}
+
+if (number==5) 
+{
+    Console.WriteLine("Friday");
+
+}
+else
+{
+    Console.WriteLine("Neither");
+}
