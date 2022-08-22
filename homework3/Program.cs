feae47cfd52eff65f746316b4e77b7c6f640d048
Inadvertensy <bobra2@ya.ru>
2022-08-22 20:35:09 +0300

    Homework new added

---
+++ homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+for (int i=2; i<=number1; i+=2)
+{
+    System.Console.Write(i+" ");
+}
+
+Console.WriteLine ("new cycle");
+
+for (int i=1;i<=number1; ++i)
+{
+    if (i%2==0) System.Console.Write(i+" ");
+}
+
+System.Console.WriteLine($"number1{number1++}");
